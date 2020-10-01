db5a011642b306bfcdaff203862f3cf80b436339
unknown <dharmamech3695@gmail.com>
2020-10-01 17:11:41 +0530

    build dir

---
+++ build/precache-manifest.40449d8eb9ccf08c0355ee91829d5e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe00cce73ced7b57b25432e154b69af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82846c55d321463dd7ee",
+    "url": "./static/css/main.e3cb06a0.chunk.css"
+  },
+  {
+    "revision": "88ae0e8186740a9f9a71",
+    "url": "./static/js/2.85535f7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.85535f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82846c55d321463dd7ee",
+    "url": "./static/js/main.82a9a953.chunk.js"
+  },
+  {
+    "revision": "dc62ceb1deb73cc66033",
+    "url": "./static/js/runtime-main.61c68b72.js"
+  }
+]);
